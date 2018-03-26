1f41e0d97748972b3ca1fce2d5bc836cbacb6df5
Lakshmanan Arunachalam <laarunac@oradev.oraclecorp.com>
2018-03-26 22:22:34 +0530

    Decompiler commit

---
+++ coolblue/decompiler/src/main/java/com/tools/decompiler/util/CustomURLClassLoader.java
@@ -0,0 +1,70 @@
+package com.tools.decompiler.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class CustomURLClassLoader extends URLClassLoader{
+
+	public CustomURLClassLoader(URL[] arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Class loadClass(String name, boolean resolve) throws ClassNotFoundException
+	{
+		 // respect the java.* packages.
+	    if( name.startsWith("java.")) {
+	      return super.loadClass(name, resolve);
+	    }
+	    else {
+	      // see if we have already loaded the class.
+	      Class<?> c = findLoadedClass(name);
+	      if( c != null ) return c;
+
+	      // the class is not loaded yet.  Since the parent class loader has all of the
+	      // definitions that we need, we can use it as our source for classes.
+	      System.out.println(name);
+	      //System.out.println(name.replaceAll("\\.", "/"));
+	      InputStream in = null;
+	      try {
+	        // get the input stream, throwing ClassNotFound if there is no resource.
+	        in = new FileInputStream("d:/" + name.replaceAll("\\.", "/") +".class");
+	        if( in == null ) throw new ClassNotFoundException("Could not find "+name);
+	        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+	        int nRead;
+	        byte[] data = new byte[16384];
+
+	        while ((nRead = in.read(data, 0, data.length)) != -1) {
+	          buffer.write(data, 0, nRead);
+	        }
+
+	        buffer.flush();
+	        // read all of the bytes and define the class.
+	        byte[] cBytes = buffer.toByteArray();
+	        c = defineClass(name, cBytes, 0, cBytes.length);
+	        System.out.println("Class loaded " + c.getName());
+	        if( resolve ) resolveClass(c);
+	        return c;
+	      } catch (Exception e) {
+	        throw new ClassNotFoundException("Could not load "+name, e);
+	      }
+	      finally {
+	    	  try
+	    	  {
+	        in.close();
+	    	  }
+	    	  catch (Exception exc)
+	    	  {
+	    		  exc.printStackTrace();
+	    	  }
+	      }
+	    }
+	  }
+	}
+
+
---
+++ coolblue/decompiler/src/main/java/com/tools/decompiler/util/JavaDecompiler.java
@@ -0,0 +1,79 @@
+package com.tools.decompiler.util;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+
+import com.strobel.decompiler.Decompiler;
+import com.strobel.decompiler.PlainTextOutput;
+
+public class JavaDecompiler {
+
+	public static String decompile() throws ClassNotFoundException, IOException {
+		PlainTextOutput output = new PlainTextOutput();
+		File file = new File("d:\\");
+		File aFile = new File("AbstractSessionManager.class");
+		// convert the file to URL format
+		URL url = file.toURI().toURL();
+		URL[] urls = new URL[] { url };
+
+		// load this folder into Class loader
+		CustomURLClassLoader cl = new CustomURLClassLoader(urls);
+		String directoryPath = "";
+		try {
+			Class cls = cl.loadClass("AbstractSessionManager", false);
+		} catch (Error exc) {
+			String msg = exc.getMessage();
+			System.out.println(msg);
+			directoryPath = msg.substring(msg.indexOf(':')+2, msg.lastIndexOf('/'));
+		}
+		System.out.println(directoryPath);
+		File directory = createDirectory(directoryPath);
+		String copyTo = directory.getAbsolutePath() + File.separatorChar + aFile.getName();
+		System.out.println(copyTo);
+		aFile.renameTo(new File(copyTo));
+		Decompiler.decompile("oracle.security.am.engines.sme.mgr.AbstractSessionManager", output);
+		File decompiled = new File("AbstractSessionManager.java");
+		FileWriter fileWriter = new FileWriter(decompiled);
+		String toPrint = output.toString();
+		BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+		int bufSize = 1024;
+		int startPos = 0;
+		System.out.println(toPrint.length());
+		try {
+			while ((startPos < toPrint.length())) {
+				System.out.println(toPrint.length());
+				if ((startPos + bufSize) >= toPrint.length()) {
+					bufSize = toPrint.length() - 1 - startPos;
+				}
+				bufWriter.write(toPrint, startPos, bufSize);
+				startPos = startPos + bufSize + 1;
+
+				bufWriter.flush();
+				System.out.println("Flushed Buffer");
+			}
+			bufWriter.close();
+		} catch (Exception exc) {
+			System.out.println(exc.getMessage());
+			exc.printStackTrace();
+		}
+		return "Good";
+	}
+
+	private static File createDirectory(String path) {
+		File directoryPath = new File(path);
+		directoryPath.mkdirs();
+		return directoryPath;
+	}
+
+	public static void main(String[] args) throws Exception {
+		try {
+			System.out.println(decompile());
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+}
